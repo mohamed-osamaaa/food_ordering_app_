74b2986e404985bdca1df3c8c9871f3b52f440f5
Mohamed Osama <141563962+mohamed-osamaaa@users.noreply.github.com>
2025-06-13 15:46:47 +0300

    fixBug

--- client/src/app/admin/menu-items/page.tsx
+++ client/src/app/admin/menu-items/page.tsx
@@ -144,7 +144,7 @@ export default function ItemsPage() {
             </div>
 
             {showForm && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+                <div className="fixed inset-0 backdrop flex items-center justify-center z-50 p-4">
                     <form
                         onSubmit={handleSubmit}
                         className="bg-white p-6 rounded-lg w-full max-w-lg space-y-4 relative overflow-y-auto max-h-[90vh]"
